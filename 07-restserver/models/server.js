1ffc63e59e26f3d6660688b27793a28dddde4830
Daniel Navarrete <dnavarrete@emergya.com>
2024-09-26 16:45:17 +0200

    Working in section 8

--- 07-restserver/models/server.js
+++ 07-restserver/models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 
 class Server {
@@ -8,11 +9,18 @@ class Server {
     this.port = process.env.PORT;
     this.usuariosPath = '/api/usuarios';
 
+    // Conectar base de datos
+    this.conectarDb();
+
     // Middlewares
     this.middlewares();
 
     // Rutas
     this.routes();
+  }
+
+  async conectarDb() {
+    await dbConnection();
   }
 
   middlewares() {
